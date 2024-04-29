7f8d4f3aefea42c49b03acb541b77c5f4242189f
Pedro Cezarini <135235828+PedroCezarini@users.noreply.github.com>
2024-04-29 11:08:12 -0300

    Create aoc23_2.py
    começo da resoluçao do ex 2 advent of code 2023

---
+++ aoc23_2.py
@@ -0,0 +1,24 @@
+#12 red cubes , 13 green cubes , 14 blue cubes
+#which games are possible with these options?
+arq = open("gameInput.txt")
+
+for linha in arq:
+    separaJogos = linha.split(';')
+
+    for jogo in separaJogos:
+        separaCor = jogo.split(",")
+
+        for cor in separaCor:
+            
+
+        #50 minutos pra ensinar tudo que o ed mostrou com o laravel aula passada kkkkkk
+        
+    
+
+
+    
+    print("")
+    
+'''
+a
+'''
