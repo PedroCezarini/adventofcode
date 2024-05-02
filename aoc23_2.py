09541053abc2020d8447fa13bb3581bd44c0777d
PedroCezarini <pedro.cezarini@fatec.sp.gov.br>
2024-05-02 20:11:34 -0300

    ta funcionando so ajustar posiçoes

--- aoc23_2.py
+++ aoc23_2.py
@@ -2,42 +2,69 @@
 #which games are possible with these options?
 #a manha nesse eh dividir com o split
 
-#agora converter o numero q sai em numeroCor[1] em int, e somar tipo azul = azul + numeroCor[1]
+# 
 arq = open("gameInput.txt")
 i = int(1)
-j = int(1)
+idSum = int(0)
+erro = int(0)
 
 for linha in arq:                                                               # Cria uma lista em q cada linha do arq eh um indice
+    linha=linha.rstrip()
     linhaSeparada = linha.split(':')
-    
-    azul = int(0)
-    verde = int(0)
-    vermemlho = int(0)
-    
+
     jogos = linhaSeparada[1].split(';')                                         # Jogos eh uma lista, onde cada rodada separada por ; eh um elemento dela.
     print("Jogo ",i)
     i=i+1
+    idSum = idSum + i
     
     for rodada in jogos:
         jogadas = rodada.split(',')                                             # Jogadas eh uma lista onde cada indice eh um numero e cor
-        print()        
+        print()     
+        
+        azul = int(0)
+        verde = int(0)
+        vermelho = int(0)   
         
         for turno in jogadas:
             numeroCor = turno.split(' ')
             print(numeroCor)
             
-            if numeroCor[2] == 'red':
-                print("Tem vermelho")
+            if numeroCor[2] == 'red':          
+                vermelho = int(vermelho) + int(numeroCor[1])
+                
             if numeroCor[2] == 'green':
-                print("Tem verde")
+                verde = int(verde) + int(numeroCor[1])
+                
             if numeroCor[2] =='blue':
-                print("Tem azul")
+                azul = int(azul) + int(numeroCor[1])
+
+        if azul > 14 or verde > 13 or vermelho > 12:
+            print("Jogo não é possível")
+            
+            #add contador no não é possível, se no fim de cada game esse contador > 0 não soma o i?
+            
+            
+        else:
+            print("Jogo é possível!!")
+        
+        print("Azul: ", azul)
+        print("Verde: ", verde)
+        print("Vermelho: ", vermelho)
+        print("Aqui meu i tem que valer o mesmo depois de Jogo", i-1)
+    
+    print("Aqui meu i já teria q valer o idSum correto?")
+    
+
+                    
+    
+        
          
         
         
     print('---------------------------')
     
-    
+idSum = idSum + i
+print (idSum)
 
 
 
